e9a8e39c542961366c9b969d2752b85e9eb7181d
ali32fps-cloud <239955998+alimohammadkarami@users.noreply.github.com>
2026-08-01 16:05:40 +0330

    return

--- src/barayannd.ir/common.js
+++
@@ -1,4 +0,0 @@
-export const _q = q => document.querySelector(q);
-export const _qa = q => document.querySelectorAll(q);
-export const _id = id => document.getElementById(id);
-export const _class = cl => document.getElementsByClassName(cl);
